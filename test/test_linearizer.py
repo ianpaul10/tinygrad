172b748537ff5883382a5c6f3b7170b9696ad45e
Ian Paul <iandouglaspaul@gmail.com>
2024-08-05 22:35:59 +0700

    Wrap all stores in if if the last uop is of type bool

--- test/test_linearizer.py
+++ test/test_linearizer.py
@@ -1857,23 +1857,23 @@ class TestKernelOpts(unittest.TestCase):
       [Opt(OptOps.PADTO, 0, 32)],
       [Opt(OptOps.PADTO, 0, 32), Opt(OptOps.UPCAST, 0, 8),],
     ])
-    # helper_linearizer_opt(a.max(1), [
-    #   [Opt(OptOps.PADTO, 0, 32)],
-    #   [Opt(OptOps.PADTO, 0, 32), Opt(OptOps.UPCAST, 0, 8),],
-    # ])
+    helper_linearizer_opt(a.max(1), [
+      [Opt(OptOps.PADTO, 0, 32)],
+      [Opt(OptOps.PADTO, 0, 32), Opt(OptOps.UPCAST, 0, 8),],
+    ])
 
-    # # cannot pad max kernel on reduce
-    # with self.assertRaises(KernelOptError):
-    #   helper_linearizer_opt(a.max(), [[Opt(OptOps.PADTO, 0, 32)],])
-    # with self.assertRaises(KernelOptError):
-    #   helper_linearizer_opt(a.max(0), [[Opt(OptOps.PADTO, 1, 32)],])
+    # cannot pad max kernel on reduce
+    with self.assertRaises(KernelOptError):
+      helper_linearizer_opt(a.max(), [[Opt(OptOps.PADTO, 0, 32)],])
+    with self.assertRaises(KernelOptError):
+      helper_linearizer_opt(a.max(0), [[Opt(OptOps.PADTO, 1, 32)],])
 
   def test_padto_where(self):
     Tensor.manual_seed(0)
     N = 17 * 17
     a = (Tensor.randn(N, N).realize().max(axis=0, keepdim=True) > 1).where(1, 0)
     helper_linearizer_opt(a.max(0), [
-      [Opt(OptOps.PADTO, 0, 32)],
+      # [Opt(OptOps.PADTO, 0, 32)],
       [Opt(OptOps.PADTO, 0, 32), Opt(OptOps.UPCAST, 0, 8),],
     ])
 
