95d4cd363c7b89a4b899a6c24d3ebfcaa2d719e4
Ian Paul <iandouglaspaul@gmail.com>
2024-09-06 08:24:25 +0900

    Merge if gates

--- tinygrad/codegen/uopgraph.py
+++ tinygrad/codegen/uopgraph.py
@@ -444,8 +444,30 @@ def delete_redundant_gates(root:UOp) -> Optional[UOp]:
   def find_gate(x:UOp) -> Optional[UOp]:
     if x.op is UOps.IF: return x
     return next((ret for s in x.src if (ret:=find_gate(s)) is not None), None)
-  if len(root.src) == 3 or (gate:=find_gate(root)) is None or gate.src[0] is not root.src[3]: return None
-  return UOp(UOps.STORE, root.dtype, root.src[:3], root.arg)
+  if len(root.src) == 4 and (sub_gate:=find_gate(root.src[2])) and sub_gate.src[0] is root.src[-1].src[0]:
+    return UOp(UOps.STORE, root.dtype, root.src[:3], root.arg)
+  return None
+
+def update_gates(root:UOp) -> Optional[UOp]:
+  if len(root.src) < 4 or root.src[3].op is UOps.IF: return None
+  return UOp(UOps.STORE, root.dtype, root.src[:3] + (UOp(UOps.IF, None, (root.src[3], root.src[2])),), root.arg)
+
+def merge_gates(sink:UOp) -> Optional[UOp]:
+  @functools.lru_cache(None)
+  def has_range(x:UOp) -> bool: return any(s2p.op is UOps.RANGE for s2p in x.sparents)
+  if len([x.op is UOps.STORE and len(x.src) == 4 for x in sink.src]) <= 1: return None
+  if_src_to_if_op: Dict[UOp, UOp] = {}
+  for x in sink.src:
+    if x.op is UOps.STORE and len(x.src) == 4 and has_range(x.src[2]) and len(x.src[-1].src) <= 2:
+      if_to_update = if_src_to_if_op.get(x.src[-1].src[0], x.src[-1])
+      if_src_to_if_op[x.src[-1].src[0]] = UOp(UOps.IF, None, (if_to_update.src[0],) + if_to_update.src[1:] + (x.src[2],), None)
+  if len(if_src_to_if_op) == 0: return None
+  new_sink_srcs = []
+  for x in sink.src:
+    if x.op is UOps.STORE and len(x.src) == 4:
+      new_sink_srcs.append(UOp(UOps.STORE, x.dtype, x.src[:-1] + (if_src_to_if_op[x.src[-1].src[0]],), x.arg))
+    else: new_sink_srcs.append(x)
+  return UOp(UOps.SINK, sink.dtype, tuple(new_sink_srcs), sink.arg)
 
 reducer = PatternMatcher([
   (NOp(UOps.REDUCE, name="root"), do_reduce),
@@ -455,6 +477,8 @@ reducer = PatternMatcher([
   (NOp(UOps.STORE, name="root"), delete_redundant_gates),
   # late fixup of unfoldable image loads
   (UPat(UOps.LOAD, src=(UPat(name="buf"), UPat()), allow_any_len=True, name="load"), fix_unfoldable_image_load),
+  (NOp(UOps.STORE, name="root"), update_gates),
+  (NOp(UOps.SINK, name="sink"), merge_gates),
 ])
 
 no_pyint = PatternMatcher([(UPat({UOps.CONST, UOps.ALU, UOps.SPECIAL, UOps.RANGE, UOps.EXPAND}, dtype=dtypes.pyint, name="x"),
